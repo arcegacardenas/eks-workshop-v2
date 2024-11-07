3f32ce7b54304474881e7997950b5730184a1991
Robin <robisohn@amazon.com>
2024-11-07 09:57:43 -0500

    deleted comments in vars.tf for worker node troubleshooting modules

--- manifests/modules/troubleshooting/workernodes/three/.workshop/terraform/vars.tf
+++ manifests/modules/troubleshooting/workernodes/three/.workshop/terraform/vars.tf
@@ -1,15 +1,3 @@
-
-# /* variable "ami_release_version" {
-#   description = "Default EKS AMI release version for node groups"
-#   type        = string
-#   #  default     = "1.30.0-20240625"
-# <<<<<<< Updated upstream
-# } */
-# =======
-# }
-# >>>>>>> Stashed changes
-
-
 # tflint-ignore: terraform_unused_declarations
 variable "eks_cluster_id" {
   description = "EKS cluster name"
